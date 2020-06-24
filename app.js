5efe14b75b8d29f51fa05b37d5c00c5341184061
Nikola Pantelic <0nikolapantelic0@gmail.com>
2020-06-24 20:16:06 +0200

    adding of state variable

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
-let scores, roundScore, activePlayer;
-const diceDOM = document.querySelector(".dice");
+let scores,         //  score variable
+    roundScore,     //  round score before hold
+    activePlayer,   //  active player variable which is change value between 1 and 0
+    gamePlaying,    //  state variable
+    diceDOM;        //  dice in DOM
+
 // start of game
 initGame();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-    // random number
-    let dice = Math.floor(Math.random() * 6) + 1;
-    // display dice result
-    diceDOM.style.display = "block";
-    diceDOM.src = "./assets/dices/dice-" + dice + ".png";
+    if (gamePlaying) {
+        // random number
+        let dice = Math.floor(Math.random() * 6) + 1;
+        // display dice result
+        diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "block";
+        diceDOM.src = "./assets/dices/dice-" + dice + ".png";
 
-    // update round score only if round number is not 1
-
-    if (dice !== 1) {
-        // add to score
-        roundScore += dice;
-        document.getElementById("current-" + activePlayer).textContent = roundScore;
-    } else {
-        // next player
-        nextPlayer();
-        // hiding the dice with timeout so that result of 1 can be visible for a short time
-        setTimeout(function () {
-            diceDOM.style.display = "none";
-        }, 500);
+        // update round score only if round number is not 1
+        if (dice !== 1) {
+            // add to score
+            roundScore += dice;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+        } else {
+            // next player
+            nextPlayer();
+            // hiding the dice with timeout so that result of 1 can be visible for a short time
+            setTimeout(function () {
+                diceDOM.style.display = "none";
+            }, 500);
+        }
     }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-    // add current score to the global score
-    scores[activePlayer] += roundScore;
-    // update the ui
-    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    if (gamePlaying) {
+        // add current score to the global score
+        scores[activePlayer] += roundScore;
+        // update the ui
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
-    // check if active player won the game
-    if (scores[activePlayer] >= 10) {
-        document.getElementById("name-" + activePlayer).textContent = "WINNER !!!";
-        diceDOM.style.display = "none";
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
-        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-    } else {
-        // next player
-        nextPlayer();
+        // check if active player won the game
+        if (scores[activePlayer] >= 100) {
+            document.getElementById("name-" + activePlayer).textContent = "WINNER !!!";
+            diceDOM.style.display = "none";
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+            gamePlaying = false;
+        } else {
+            // next player
+            nextPlayer();
+        }
     }
 });
 
@@ -50,10 +59,10 @@ function initGame() {
     // names of players
     let player1 = prompt("Please enter name for Player 1", "Player 1");
     let player2 = prompt("Please enter name for Player 2", "Player 2");
-    if (player1 !== null && player2 !== null){
+    if (player1 !== null && player2 !== null) {
         document.getElementById("name-0").textContent = player1;
         document.getElementById("name-1").textContent = player2;
-    }else {
+    } else {
         document.getElementById("name-0").textContent = "Player 1";
         document.getElementById("name-1").textContent = "Player 2";
     }
@@ -62,13 +71,13 @@ function initGame() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     document.querySelector(".dice").style.display = "none";
     document.getElementById("score-0").textContent = "0";
     document.getElementById("score-1").textContent = "0";
     document.getElementById("current-0").textContent = "0";
     document.getElementById("current-1").textContent = "0";
-
     document.querySelector(".player-0-panel").classList.remove("active");
     document.querySelector(".player-1-panel").classList.remove("active");
     document.querySelector(".player-0-panel").classList.remove("winner");
